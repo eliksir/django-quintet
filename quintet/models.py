3a46a5532a797522541d782fbea7293f4efe9070
Simen Heggestøyl <simen@e5r.no>
2015-06-30 15:35:57 +0200

    Make Django-Quintet compatible with Django 1.8
    Since Django 1.8, the options `auto_now` and `auto_now_add` are mutually
    exclusive; combining the two results in an error. Remove `auto_now_add`
    from `TimeStampedModel` in order to stay compatible with Django 1.8.

--- quintet/models.py
+++ quintet/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class TimeStampedModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now_add=True, auto_now=True)
+    modified = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
